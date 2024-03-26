889e094bc3593de211625e79edd0de0b51b8ec70
Your Name <your_email@whatever.com>
2024-03-26 15:19:35 +0300

    add lab12

---
+++ lr12/src/main/java/org/example/main/Main.java
@@ -0,0 +1,8 @@
+package org.example.main;
+
+public class Main {
+    public static void main(String[] args) {
+        Tester.greetTester();
+        System.out.println( "Hello, Сергей!" );
+    }
+}
